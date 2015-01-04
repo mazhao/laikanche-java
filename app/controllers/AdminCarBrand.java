b93e3c933fe19883ddb648c8a6eecda869b17818
Ma Zhao <ma.zhao@dl.cn>
2015-01-04 15:16:01 +0800

    简化完成

--- app/controllers/AdminCarBrand.java
+++ app/controllers/AdminCarBrand.java
@@ -25,84 +25,84 @@ public class AdminCarBrand extends Controller {
         return ok(views.html.adminCarBrand.render(carBrandList));
     }
 
-
-    public static Result gotoAdd() {
-        return ok(views.html.adminCarBrandAdd.render(carBrandFormFactory));
-    }
-
-    public static Result add() {
-        // 1. 添加一个新的车品牌
-
-
-        CarBrandForm newCarBrandForm = carBrandFormFactory.bindFromRequest().get();
-
-        // form validation goes here
-
-        CarBrand carBrand = new CarBrand();
-        carBrand.name = newCarBrandForm.name;
-        carBrand.description = newCarBrandForm.description;
-
-        Ebean.save(carBrand);
-
-        // 2. 重新回到列表页
-        return redirect(routes.AdminCarBrand.index());
-
-    }
-
-
-    public static Result gotoEdit(Long id) {
-
-        // 从数据库查询原值
-        CarBrand carBrand = Ebean.find(CarBrand.class, id);
-
-        // 用原值建造Form
-        CarBrandForm cbf = new CarBrandForm();
-        cbf.id = id;
-        cbf.name = carBrand.name;
-        cbf.description = carBrand.description;
-
-        // 把Form展现到页面。
-        return ok(views.html.adminCarBrandEdit.render(carBrandFormFactory.fill(cbf)));
-    }
-
-    public static Result edit(Long id) {
-
-        // 使用表单中的数据更新数据库中的车品牌。
-        CarBrandForm carBrandForm = carBrandFormFactory.bindFromRequest().get();
-
-        CarBrand carBrand = Ebean.find(CarBrand.class, id);
-        carBrand.name = carBrandForm.name;
-        carBrand.description = carBrandForm.description;
-
-        Ebean.update(carBrand);
-
-        // 2. 重新回到列表页
-        return redirect(routes.AdminCarBrand.index());
-
-    }
-
-
-    public static Result detail(Long id) {
-
-        // 从数据库查询原值
-        CarBrand carBrand = Ebean.find(CarBrand.class, id);
-
-        // 用原值建造Form
-        CarBrandForm cbf = new CarBrandForm();
-        cbf.id = id;
-        cbf.name = carBrand.name;
-        cbf.description = carBrand.description;
-
-        // 把Form展现到页面。
-        return ok(views.html.adminCarBrandDetail.render(carBrandFormFactory.fill(cbf)));
-    }
-
-    public static Result delete(Long id) {
-
-        Ebean.delete(CarBrand.class, id);
-
-        return redirect(routes.AdminCarBrand.index());
-    }
+//
+//    public static Result gotoAdd() {
+//        return ok(views.html.adminCarBrandAdd.render(carBrandFormFactory));
+//    }
+//
+//    public static Result add() {
+//        // 1. 添加一个新的车品牌
+//
+//
+//        CarBrandForm newCarBrandForm = carBrandFormFactory.bindFromRequest().get();
+//
+//        // form validation goes here
+//
+//        CarBrand carBrand = new CarBrand();
+//        carBrand.name = newCarBrandForm.name;
+//        carBrand.description = newCarBrandForm.description;
+//
+//        Ebean.save(carBrand);
+//
+//        // 2. 重新回到列表页
+//        return redirect(routes.AdminCarBrand.index());
+//
+//    }
+//
+//
+//    public static Result gotoEdit(Long id) {
+//
+//        // 从数据库查询原值
+//        CarBrand carBrand = Ebean.find(CarBrand.class, id);
+//
+//        // 用原值建造Form
+//        CarBrandForm cbf = new CarBrandForm();
+//        cbf.id = id;
+//        cbf.name = carBrand.name;
+//        cbf.description = carBrand.description;
+//
+//        // 把Form展现到页面。
+//        return ok(views.html.adminCarBrandEdit.render(carBrandFormFactory.fill(cbf)));
+//    }
+//
+//    public static Result edit(Long id) {
+//
+//        // 使用表单中的数据更新数据库中的车品牌。
+//        CarBrandForm carBrandForm = carBrandFormFactory.bindFromRequest().get();
+//
+//        CarBrand carBrand = Ebean.find(CarBrand.class, id);
+//        carBrand.name = carBrandForm.name;
+//        carBrand.description = carBrandForm.description;
+//
+//        Ebean.update(carBrand);
+//
+//        // 2. 重新回到列表页
+//        return redirect(routes.AdminCarBrand.index());
+//
+//    }
+//
+//
+//    public static Result detail(Long id) {
+//
+//        // 从数据库查询原值
+//        CarBrand carBrand = Ebean.find(CarBrand.class, id);
+//
+//        // 用原值建造Form
+//        CarBrandForm cbf = new CarBrandForm();
+//        cbf.id = id;
+//        cbf.name = carBrand.name;
+//        cbf.description = carBrand.description;
+//
+//        // 把Form展现到页面。
+//        return ok(views.html.adminCarBrandDetail.render(carBrandFormFactory.fill(cbf)));
+//    }
+//
+//    public static Result delete(Long id) {
+//
+//        Ebean.delete(CarBrand.class, id);
+//
+//        return redirect(routes.AdminCarBrand.index());
+//    }
 
 
     // one action for save, update and delete
