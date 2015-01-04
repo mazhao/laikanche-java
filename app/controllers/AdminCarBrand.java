22f40a8d74703d5f4fbe98a43b8334f2d812d78d
Ma Zhao <ma.zhao@dl.cn>
2015-01-04 14:33:39 +0800

    简化成单张页面基本OK，还需要处理页面展现细节。

--- app/controllers/AdminCarBrand.java
+++ app/controllers/AdminCarBrand.java
@@ -6,6 +6,7 @@ import models.*;
 import play.*;
 import play.data.Form;
 import play.mvc.*;
+import utils.Constraints;
 
 import java.util.List;
 
@@ -26,7 +27,7 @@ public class AdminCarBrand extends Controller {
 
 
     public static Result gotoAdd() {
-        return ok(views.html.adminCarBrandAdd.render( carBrandFormFactory ));
+        return ok(views.html.adminCarBrandAdd.render(carBrandFormFactory));
     }
 
     public static Result add() {
@@ -101,6 +102,71 @@ public class AdminCarBrand extends Controller {
         Ebean.delete(CarBrand.class, id);
 
         return redirect(routes.AdminCarBrand.index());
+    }
+
+
+    // one action for save, update and delete
+    public static Result read(Long id, String operation) {
+
+        if (operation == null) {
+            operation = Constraints.OP_ERROR;
+        }
+
+        // 用原值建造Form
+        CarBrandForm cbf = new CarBrandForm();
+
+        if (Constraints.OP_DELETE.equalsIgnoreCase(operation) ||
+                Constraints.OP_UPDATE.equalsIgnoreCase(operation) ||
+                Constraints.OP_READ.equalsIgnoreCase(operation)
+                ) { // update, delete, read
+            // 从数据库查询原值
+            CarBrand carBrand = Ebean.find(CarBrand.class, id);
+
+            cbf.id = id;
+            cbf.name = carBrand.name;
+            cbf.description = carBrand.description;
+
+            cbf.operationCode = operation;
+        } else if (operation.equalsIgnoreCase(Constraints.OP_CREATE)) { // create
+            cbf.operationCode = Constraints.OP_CREATE;
+        } else {
+            return redirect(routes.AdminCarBrand.index()); // @todo with error message
+        }
+
+        return ok(views.html.adminCarBrandRead.render(carBrandFormFactory.fill(cbf)));
+    }
+
+    public static Result saveOrUpdateOrDelete() {
+
+        CarBrandForm carBrandForm = carBrandFormFactory.bindFromRequest().get();
+
+        if (Constraints.OP_CREATE.equalsIgnoreCase(carBrandForm.operationCode)) {
+            // create
+            // form validation goes here
+
+            CarBrand carBrand = new CarBrand();
+            carBrand.name = carBrandForm.name;
+            carBrand.description = carBrandForm.description;
+
+            Ebean.save(carBrand);
+
+        } else if (Constraints.OP_DELETE.equalsIgnoreCase(carBrandForm.operationCode)) {
+            Ebean.delete(CarBrand.class, carBrandForm.id);
+
+        } else if (Constraints.OP_UPDATE.equalsIgnoreCase(carBrandForm.operationCode)) {
+            CarBrand carBrand = Ebean.find(CarBrand.class, carBrandForm.id);
+            carBrand.name = carBrandForm.name;
+            carBrand.description = carBrandForm.description;
+
+            Ebean.update(carBrand);
+        } else {
+            // error
+            return redirect(routes.AdminCarBrand.index()); // @todo with error message
+
+        }
+
+        return redirect(routes.AdminCarBrand.index());
+
     }
 
 }
