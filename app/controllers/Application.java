749a329dfb961d691865d0eb5be3443210ad6d6f
Ma Zhao <ma.zhao@dl.cn>
2015-01-13 00:04:47 +0800

    首页添加视频

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import models.CarVideo;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+
+        List<CarVideo> carVideoList = CarVideo.find.all();
+
+
+
+        return ok(index.render(carVideoList));
     }
 
 }
